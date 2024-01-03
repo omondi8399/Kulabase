a19a0d73bbcbc7a828bed0143e1e817deab58119
Rodgers Omondi <rodgersomondi8399@gmail.com>
2024-01-03 18:54:56 +0300

    updated register page

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,15 +1,29 @@
+"use client"
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
 const RegisterPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    function handleFormSubmit(ev) {
+        ev.preventDefault();
+        fetch('/api/register', {
+            method: 'POST',
+            body: JSON.stringify({email, password}),
+            headers: {'Content-Type': 'application/json'},
+        })
+    }
 return (
     <section>
         <h1 className='text-center text-primary text-4xl mb-4'>
             Register
         </h1>
-        <form className='block max-w-xs mx-auto'>
-            <input type='email' placeholder='email' />
-            <input type='password' placeholder='password' />
+        <form className='block max-w-xs mx-auto' onSubmit={handleFormSubmit}>
+            <input type='email' placeholder='email' value={email}
+                onChange={ev => setEmail(ev.target.value)} />
+            <input type='password' placeholder='password' value={password}
+                onChange={ev => setPassword(ev.target.value)} />
             <button type='submit'>Register</button>
             <div className='my-4 text-center text-gray-500'>
                 or login with provider
