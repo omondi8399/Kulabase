a5c4bde2a571402fb0b8d93599e7c6370994ebb4
Rodgers Omondi <rodgersomondi8399@gmail.com>
2024-01-04 14:07:06 +0300

    Updated Hero Layout component

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -19,11 +19,11 @@ import Right from '../icons/Right'
                     complete, a simple yet delicious joy in life.
                 </p>
                 <div className='flex gap-4 text-sm'>
-                    <button className='bg-primary uppercase flex gap-2 text-white px-4 py-2 items-center rounded-full'>
+                    <button className='bg-primary uppercase flex justify-center gap-2 text-white px-4 py-2 items-center rounded-full'>
                         Order now
                         <Right />
                     </button>
-                    <button className='flex gap-2 text-gray-600 font-semibold'>
+                    <button className='flex items-center gap-2 border-0 text-gray-600 font-semibold'>
                         Learn more
                         <Right />
                     </button>
