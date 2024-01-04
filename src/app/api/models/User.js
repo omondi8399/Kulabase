f6fefff6c0739e8c3da42a85b31632900176b517
Rodgers Omondi <rodgersomondi8399@gmail.com>
2024-01-04 14:11:03 +0300

    Updated user model

--- src/app/api/models/User.js
+++ src/app/api/models/User.js
@@ -4,26 +4,10 @@ import { model, models,Schema } from "mongoose";
 
 const UserSchema = new Schema({
     email: {type: String, required: true, unique: true},
-    password: {
-        type: String,
-        required: true, 
-        validate: pass => {
-            if(!pass?.length || pass.length < 5) {
-                new Error('password must be atleast 5 characters');
-                return false;
-            }
-        },
-    },
+    password: {type: String},
 
 }, {timestamps: true});
 
-UserSchema.post('validate', function (user) {
-    const notHashedPassword = user.password;
-    const salt = bcrypt.genSaltSync(10);
-    user.password = bcrypt.hashSync(notHashedPassword, salt);
-
-})
 
 export const User = models?.User || model('User', UserSchema);
 
-export default User;
