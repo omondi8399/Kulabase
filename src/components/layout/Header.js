10fae0d886ade3bc28d836d5a1287ae824180a35
Rodgers Omondi <rodgersomondi8399@gmail.com>
2024-01-04 14:08:01 +0300

    Updated Header Layout component

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,15 @@
-    import Link from 'next/link'
+    import {signOut, useSession } from 'next-auth/react'
+import Link from 'next/link'
     import React from 'react'
 
     const Header = () => {
+        const session = useSession()
+        const status = session?.status;
+        const userData = session.data?.user;
+        let userName = userData?.name || userData?.email;
+        if (userName && userName.includes(' ')) {
+            userName = userName.split('')[0];
+        }
     return (
         <>
             <header className='flex items-center justify-between'>
@@ -15,8 +23,21 @@
                 <Link href={''} className='bg-primary rounded-full text-white px-8 py-2'>Login</Link>
             </nav>
             <nav className='flex items-center gap-4 text-gray-500 font-semibold'>
-                <Link href={'/login'}>Login</Link>
-                <Link href={'/register'} className='bg-primary rounded-full text-white px-8 py-2'>Register</Link>
+                {status === 'authenticated' && (
+                    <>  
+                        <Link href={'/profile'} className='whitespace-nowrap'>
+                            Hello, {userName}</Link>
+                        <button onClick={() => signOut()} className='bg-primary rounded-full text-white px-8 py-2'>
+                            Logout
+                        </button>
+                    </>
+                )}
+                {status !== 'unauthenticated' && (
+                    <>
+                        <Link href={'/login'}>Login</Link>
+                        <Link href={'/register'} className='bg-primary rounded-full text-white px-8 py-2'>Register</Link>
+                    </>
+                )}
             </nav>
             </header>
         </>
