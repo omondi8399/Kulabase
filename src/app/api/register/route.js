d6e71fa2304b69e359587f95cdf8ec006d9b6644
Rodgers Omondi <rodgersomondi8399@gmail.com>
2024-01-04 14:10:07 +0300

    Updated register api

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -4,6 +4,16 @@ import User from "../models/User";
 export async function POST(req) {
     const body = await req.json();
     mongoose.connect(process.env.MONGO_URL);
+    const pass = body.password;
+    if (!pass?.length || pass.length < 5) {
+        new Error('password must be atleast 5 characters');
+    }
+
+    const notHashedPassword = pass;
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(notHashedPassword, salt);
+    body.password = hashedPassword;
+
     const createdUser = await User.create(body);
     return Response.json(createdUser);
 }
