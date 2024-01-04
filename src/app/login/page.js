9a582091fe042bdab5c6bf4aa61ee621a036c12f
Rodgers Omondi <rodgersomondi8399@gmail.com>
2024-01-04 14:04:22 +0300

    Added Login page

---
+++ src/app/login/page.js
@@ -0,0 +1,45 @@
+"use client"
+
+import { signIn } from 'next-auth/react';
+import Image from 'next/image';
+import React, { useState } from 'react'
+
+const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    const [loginInProgress, setLoginInProgress] = useState(false);
+    async function handleFormSubmit(ev) {
+        ev.preventDefault();
+        setLoginInProgress(true)
+        
+        await signIn('Credentials', {email, password, callbackUrl: '/'});
+
+        setLoginInProgress(false);
+    }
+return (
+    <section className='mt-8'>
+        <h1 className='text-center text-primary text-4xl mb-4'>
+            Login
+        </h1>
+        <form className='max-w-xs mx-auto' onSubmit={handleFormSubmit}>
+            <input type='email' name='email' placeholder='email' value={email}
+                    disabled={loginInProgress}
+                    onChange={ev => setEmail(ev.target.value)} />
+            <input type='password' name='password' placeholder='password' value={password}
+                    disabled={loginInProgress}
+                    onChange={ev => setPassword(ev.target.value)} />
+            <button disabled={loginInProgress} type='submit'>Login</button>
+            <div className='my-4 text-center text-gray-500'>
+                or login with provider
+            </div>
+            <button type='button' onClick={() => signIn('google', {callbackUrl:'/'})} 
+                className='flex gap-4 justify-center'>
+                <Image src={'/google.png'} alt={""} width={32} height={32} />
+                Login with google
+            </button>
+        </form>
+    </section>
+)
+}
+
+export default LoginPage
