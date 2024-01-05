3206ff6b438ea6978c3ebad21f07096fbbdaf969
Rodgers Omondi <rodgersomondi8399@gmail.com>
2024-01-05 10:35:31 +0300

    updated App layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Roboto } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/layout/Header'
 import AppProvider from '@/components/AppContext'
+import { Toaster } from 'react-hot-toast'
 
 const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] })
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={roboto.className}>
         <main className='max-w-4xl mx-auto p-4'>
           <AppProvider>
+            <Toaster />
             <Header />
             {children}
             <footer className='border-t p-8 text-center text-gray-500 mt-16'>
