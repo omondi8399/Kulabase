b06e749da649b833416d517cb67e37585d99805c
Rodgers Omondi <rodgersomondi8399@gmail.com>
2024-01-05 07:46:53 +0300

    updated user model

--- src/app/api/models/User.js
+++ src/app/api/models/User.js
@@ -3,6 +3,7 @@ import { model, models,Schema } from "mongoose";
 
 
 const UserSchema = new Schema({
+    name: {type: String},
     email: {type: String, required: true, unique: true},
     password: {type: String},
 
