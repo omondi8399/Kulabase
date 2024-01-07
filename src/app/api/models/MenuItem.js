e53cf7fc0fdfbca905723b3ee5ce3e645084f786
Rodgers Omondi <rodgersomondi8399@gmail.com>
2024-01-07 17:46:17 +0300

    Updated MenuItem model

--- src/app/api/models/MenuItem.js
+++ src/app/api/models/MenuItem.js
@@ -1,10 +1,18 @@
 const { Schema } = require("mongoose");
 
+const ExtraPriceSchema = new Schema({
+    name: String,
+    price: Number,
+})
+
 const MenuItemSchema = new Schema({
     image: {type: String},
     name: {type: String},
     description: {type: String},
     basePrice: {type: Number},
+    sizes: {type:[ExtraPriceSchema]},
+    extraIngredientPrices: {type:[ExtraPriceSchema]},
+
 }, {timestamps: true});
 
 export const MenuItem = models?.MenuItem || model('MenuItem', MenuItemSchema);
