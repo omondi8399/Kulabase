4a59bd9b6213234e9ba127745e1fcb1821042ebe
Rodgers Omondi <rodgersomondi8399@gmail.com>
2024-01-07 17:45:41 +0300

    Updated menu-items api

--- src/app/api/menu-items/route.js
+++ src/app/api/menu-items/route.js
@@ -7,3 +7,18 @@ export async function POST(req) {
     const menuItemDoc = await MenuItem.create(data);
     return Response.json(menuItemDoc);
 }
+
+export async function PUT(req) {
+    mongoose.connect(process.env.MONGO_URL);
+    const {_id, ...data} = await req.json();
+    await MenuItem.findByIdAndUpdate(_id, data);
+    return Response.json(true);
+
+}
+
+export async function GET() {
+    mongoose.connect(process.env.MONGO_URL);
+    return Response.json(
+        await MenuItem.find()
+    );
+}
