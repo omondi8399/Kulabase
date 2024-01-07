1d4cd2a7f00b872cb4d8f605da060c4577b6fce9
Rodgers Omondi <rodgersomondi8399@gmail.com>
2024-01-07 17:47:37 +0300

    Updated Profile api

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -2,14 +2,19 @@ import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
 import User from "../models/User";
+import { UserInfo } from "../models/UserInfo";
 
 export async function PUT(req) {
     mongoose.connect(process.env.MONGO_URL);
     const data = await req.json();
+    const {name, image, ...otherUserInfo} = data
+
     const session = await getServerSession(authOptions);
     const email = session.user.email;
     
-    await User.updateOne({email}, data);
+    await User.updateOne({email}, {name, image});
+
+    await UserInfo.findOneAndUpdate({email}, otherUserInfo, {upsert:true});
 
     return Response.json(true)
 }
@@ -17,8 +22,12 @@ export async function PUT(req) {
 export async function GET() {
     mongoose.connect(process.env.MONGO_URL);
     const session = await getServerSession(authOptions);
-    const email = session.user.email;
-    return Response.json(
-        await User.findOne({email})
-    )
+    const email = session?.user?.email;
+    if (!email) {
+        return Response.json({});
+    }
+
+    const user = await User.findOne({email}).lean();
+    const userInfo = await UserInfo.findOne({email}).lean();
+    return Response.json({...user, ...userInfo})
 }
