6100f68675bd87ad6bfd9cf5b9b3b51fe91137a6
Dev Rojas <rodgersomond8399@gmail.com>
2024-01-09 13:52:26 +0300

    Updated categories api

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -21,3 +21,11 @@ export async function GET() {
         await Category.find()
     )
 }
+
+export async function Delete(req) {
+    mongoose.connect(process.env.MONGO_URL);
+    const url = new URL(req.url);
+    const _id = url.searchParams.get('_id');
+    await Category.deleteOne({_id});
+    return Response.json(true);
+}
